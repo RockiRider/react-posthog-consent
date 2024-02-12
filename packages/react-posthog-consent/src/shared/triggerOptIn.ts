ef03af73347b7e5350345dd05f24f64c59217181
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-02-12 23:36:19 +0000

    add better config

--- packages/react-posthog-consent/src/shared/triggerOptIn.ts
+++ packages/react-posthog-consent/src/shared/triggerOptIn.ts
@@ -2,11 +2,20 @@ import { ConsentConfig, PostHog } from "./types";
 
 export const triggerOptIn = (
   posthog: PostHog | null,
-  config: ConsentConfig
+  config: ConsentConfig,
+  expirationDate?: Date
 ) => {
-  //TODO: Re-trigger opt-in capturing, with days left
-  posthog?.opt_in_capturing();
-  if (config.enable_session_recording) {
-    posthog?.startSessionRecording();
+  if (expirationDate) {
+    const daysLeft = Math.ceil(
+      (expirationDate.getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24)
+    );
+    posthog?.opt_in_capturing({
+      cookie_expiration: daysLeft,
+      capture_event_name: `Automatic ${config.opt_in_name}`,
+      capture_properties: config.capture_event_properties,
+    });
+    if (config.enable_session_recording) {
+      posthog?.startSessionRecording();
+    }
   }
 };
